c77a76b4e4936e70d4dcaf3c7b68349071251847
Andrew Turner <turner.andrew0@gmail.com>
2021-07-15 15:12:44 -0500

    update

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = express()
 
 const path = require('path')
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
